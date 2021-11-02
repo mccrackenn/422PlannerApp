b933a274a133075b8775d487b696492ec476808b
Conner Hofheins <chofheins5@gmail.com>
2021-11-02 12:04:51 -0700

    added components of angular material

--- plannerApp/src/app/app.module.ts
+++ plannerApp/src/app/app.module.ts
@@ -4,10 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
 import { LoginComponent } from './login/login.component';
 import { NotesComponent } from './notes/notes.component';
 import { ToDosComponent } from './to-dos/to-dos.component';
+
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 
 @NgModule({
@@ -21,7 +32,16 @@ import { ToDosComponent } from './to-dos/to-dos.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatToolbarModule
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatExpansionModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- plannerApp/src/app/to-dos/to-dos.component.ts
+++ plannerApp/src/app/to-dos/to-dos.component.ts
@@ -12,4 +12,5 @@ export class ToDosComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
 }
