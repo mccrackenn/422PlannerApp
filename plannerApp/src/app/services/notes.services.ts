098ecabc2b900d2e9ece7cbe34717650ef298a50
mccrackenn <mccrackenn@gmail.com>
2021-11-10 16:15:02 -0800

    Fixed my formatting problems

--- plannerApp/src/app/services/notes.services.ts
+++ plannerApp/src/app/services/notes.services.ts
@@ -1,18 +1,14 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { ActivatedRoute, Router } from "@angular/router";
-import { Subject, Subscriber, Subscription } from "rxjs";
-import { Note } from "src/app/models/note";
-
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subject, Subscriber, Subscription } from 'rxjs';
+import { Note } from 'src/app/models/note';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class NotesServices
-{
-  private notes: Note[] = [
-
-  ];
+export class NotesServices {
+  private notes: Note[] = [];
 
   notesUpdated = new Subject<Note[]>();
 
@@ -20,65 +16,61 @@ export class NotesServices
     public router: Router,
     public route: ActivatedRoute,
     public httpClient: HttpClient
-  ) { };
+  ) {}
 
-  getNotes()
-  {
+  getNotes():void {
     //Using the spread opertor to return a copy of the array, not the original array
-    this.httpClient.get<Note[]>('http://localhost:3000/api/notes')
-      .subscribe(
-        responseData =>
-        {
-          console.log(responseData)
-          this.notes = responseData
-          this.notesUpdated.next([...this.notes])
-        })
+    this.httpClient
+      .get<Note[]>('http://localhost:3000/api/notes')
+      .subscribe((responseData: Note[]) => {
+        console.log(responseData);
+        this.notes = responseData;
+        this.notesUpdated.next([...this.notes]);
+      });
   }
 
-  addNote(note: Note)
-  {
-    this.httpClient.post<{ message: string, noteId: string }>('http://localhost:3000/api/notes', note)
-      .subscribe((responseData) =>
-      {
+  addNote(note: Note) {
+    this.httpClient
+      .post<{ message: string; noteId: string }>(
+        'http://localhost:3000/api/notes',
+        note
+      )
+      .subscribe((responseData) => {
         const newNote: Note = {
           id: responseData.noteId,
           title: note.title,
           description: note.description,
           startDate: note.startDate,
           endDate: note.endDate,
-          createdDate: note.createdDate
-        }
-        console.log(newNote)
+          createdDate: note.createdDate,
+        };
+        console.log(newNote);
         this.notes.push(newNote);
-        this.notesUpdated.next([...this.notes])
+        this.notesUpdated.next([...this.notes]);
         //this.router.navigate(['/notes']).then(() => window.location.reload())
-
-      })
+      });
   }
 
-  deleteNote(noteId: string)
-  {
-    this.httpClient.delete('http://localhost:3000/api/notes/' + noteId)
-      .subscribe(() =>
-      {
-        const updatedNotes = this.notes.filter(note => note.id !== noteId);
+  deleteNote(noteId: string) {
+    this.httpClient
+      .delete('http://localhost:3000/api/notes/' + noteId)
+      .subscribe(() => {
+        const updatedNotes = this.notes.filter((note) => note.id !== noteId);
         console.log(updatedNotes);
         this.notes = updatedNotes;
-        this.notesUpdated.next([...this.notes])
+        this.notesUpdated.next([...this.notes]);
         //this.router.navigate(['/notes']).then(() => window.location.reload())
-      })
+      });
   }
 
-  getHeroesUpdateListener()
-  {
-    return this.notesUpdated.asObservable()
+  getHeroesUpdateListener() {
+    return this.notesUpdated.asObservable();
   }
 
-  getNote(id: string)
-  {
-    console.log(`Note is ${id}`)
-    const returnNote = this.notes.find(note => note.id === id)
-    console.log(`getNote return an id of-${id}`)
+  getNote(id: string) {
+    return this.httpClient.get<Note>('http://localhost:3000/api/notes/' + id);
+    // console.log(`Note is ${id}`)
+    // const returnNote = this.notes.find(note => note.id === id)
+    // console.log(`getNote return an id of-${id}`)
   }
-
 }
