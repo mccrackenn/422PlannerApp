da7cfba58faf04074884bee04b5d24bac6ba8d7b
ivytran <59583454+ivytran@users.noreply.github.com>
2021-11-10 17:16:41 -0800

    Login Working

--- plannerApp/src/app/login/login.component.ts
+++ plannerApp/src/app/login/login.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  template: `
+    <signin></signin>
+    `
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  constructor() { console.clear(); }
+}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+@Component({
+  selector: 'signin',
+  template: `<h1>{{title}} </h1>
+   <google-signin></google-signin>`
+})
+export class SignInComponent {
+  title     = "signin page";
+  constructor() { console.clear(); }
 }
